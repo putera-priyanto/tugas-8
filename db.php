ced8046a2837001681cf3f0f7ef079c3b0a05adf
putera-priyanto <81774141+putera-priyanto@users.noreply.github.com>
2021-07-11 17:31:35 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$dbname   = 'photragraph';
+
+$coon = mysqli_connect($hostname, $username, $password, $photragraph) or die ('gagal terhubung ke database');
+?>
